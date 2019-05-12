1fc07bf59612229e8f607ed5fdd632cd2ae80d82
Jose Renau <renau@ucsc.edu>
2019-05-12 14:52:06 -0700

    small changes to make it more bazel friendly

--- genfiles/GitSHA1.cpp
+++ genfiles/GitSHA1.cpp
@@ -20,44 +20,44 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ***********************************************/
 
-#include "src/GitSHA1.h"
+#include "cryptominisat5/GitSHA1.h"
 
 const char* CMSat::get_version_sha1()
 {
-    static const char myversion_sha1[] = "@GIT_SHA1@";
+    static const char myversion_sha1[] = "f5609bfda3edbe373999fa5f79bc153ee619a7ca";
     return myversion_sha1;
 }
 
 const char* CMSat::get_version_tag()
 {
-    static const char myversion_tag[] = "@PROJECT_VERSION@";
+    static const char myversion_tag[] = "5.6.8";
     return myversion_tag;
 }
 
 const char* CMSat::get_compilation_env()
 {
     static const char compilation_env[] =
-    "CMAKE_CXX_COMPILER = @CMAKE_CXX_COMPILER@ | "
-    "CMAKE_CXX_FLAGS = @CMAKE_CXX_FLAGS@ | "
-    "COMPILE_DEFINES = @COMPILE_DEFINES@ | "
-    "STATICCOMPILE = @STATICCOMPILE@ | "
-    "ONLY_SIMPLE = @ONLY_SIMPLE@ | "
-    "Boost_FOUND = @Boost_FOUND@ | "
-    "STATS = @STATS@ | "
-    "SQLITE3_FOUND = @SQLITE3_FOUND@ | "
-    "ZLIB_FOUND = @ZLIB_FOUND@ | "
-    "VALGRIND_FOUND = @VALGRIND_FOUND@ | "
-    "ENABLE_TESTING = @ENABLE_TESTING@ | "
-    "M4RI_FOUND = @M4RI_FOUND@ | "
-    "NOM4RI = @NOM4RI@ | "
-    "SLOW_DEBUG = @SLOW_DEBUG@ | "
-    "ENABLE_ASSERTIONS = @ENABLE_ASSERTIONS@ | "
-    "PYTHON_EXECUTABLE = @PYTHON_EXECUTABLE@ | "
-    "PYTHON_LIBRARY = @PYTHON_LIBRARY@ | "
-    "PYTHON_INCLUDE_DIRS = @PYTHON_INCLUDE_DIRS@ | "
-    "MY_TARGETS = @MY_TARGETS@ | "
-    "LARGEMEM = @LARGEMEM@ | "
-    "LIMITMEM = @LIMITMEM@ | "
+    "CMAKE_CXX_COMPILER = /usr/bin/c++ | "
+    "CMAKE_CXX_FLAGS =  -Wall -Wextra -Wunused -Wsign-compare -fno-omit-frame-pointer -Wtype-limits -Wuninitialized -Wno-deprecated -Wstrict-aliasing -Wpointer-arith -Wpointer-arith -Wformat-nonliteral -Winit-self -Wparentheses -Wunreachable-code -ggdb3 -Wlogical-op -Wrestrict -Wnull-dereference -Wdouble-promotion -Wshadow -Wformat=2 -Wextra-semi -pedantic -Wno-class-memaccess | "
+    "COMPILE_DEFINES =  -DUSE_GAUSS -DBOOST_TEST_DYN_LINK -DUSE_ZLIB -DYALSAT_FPU | "
+    "STATICCOMPILE = OFF | "
+    "ONLY_SIMPLE = OFF | "
+    "Boost_FOUND = 1 | "
+    "STATS = OFF | "
+    "SQLITE3_FOUND =  | "
+    "ZLIB_FOUND = TRUE | "
+    "VALGRIND_FOUND =  | "
+    "ENABLE_TESTING = OFF | "
+    "M4RI_FOUND = FALSE | "
+    "NOM4RI = OFF | "
+    "SLOW_DEBUG = OFF | "
+    "ENABLE_ASSERTIONS = ON | "
+    "PYTHON_EXECUTABLE = /usr/bin/python3 | "
+    "PYTHON_LIBRARY = /usr/lib/libpython3.7m.so | "
+    "PYTHON_INCLUDE_DIRS = /usr/include/python3.7m | "
+    "MY_TARGETS =  | "
+    "LARGEMEM = OFF | "
+    "LIMITMEM = OFF | "
     "compilation date time = " __DATE__ " " __TIME__
     ""
     ;
